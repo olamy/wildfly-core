c6fb172de68cb0011758320fe6fdf1d20a19636d
Emmanuel Hugonnet <emmanuel.hugonnet@gmail.com>
2017-09-29 17:15:24 +0200

    [model-diff]:  refactoring moving some code to the controller      * Moving common code to controller      * Preparing standalone move      * Preparing standalone synchronization      * Standalone sync      * Marking the read modle operations as public because of WFCORE-13      * Adding support for admin-mode and some more traces.      * Adding support for dry-run which will return the list of operations.      * Removal of extra extensions      * Adding export-diff command to export the configuration difference as a cli script      * Removing batch and obtaining the server name.

--- host-controller/src/main/java/org/jboss/as/host/controller/DomainModelControllerService.java
+++ host-controller/src/main/java/org/jboss/as/host/controller/DomainModelControllerService.java
@@ -942,7 +942,7 @@ public class DomainModelControllerService extends AbstractControllerService impl
                 contentRepository,
                 ignoredRegistry,
                 new DomainModelControllerService.InternalExecutor(),
-                this,
+                expressionResolver,
                 environment,
                 getExecutorServiceInjector().getValue(),
                 currentRunningMode,
